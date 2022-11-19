de202ce7add80c414738f815602e76b7508798ac
Franck <franck@open-time.net>
2022-11-19 10:56:49 +0100

    locales

---
+++ locales/en/main.lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Room'] = '';
+l10n::$locales['Check-in'] = '';
+l10n::$locales['Check-out'] = '';
+l10n::$locales['Staff role'] = '';
+l10n::$locales['%Y-%m-%d'] = '';
+l10n::$locales['Auberge'] = '';
+l10n::$locales['Room number:'] = '';
+l10n::$locales['0 = not set, 1 to 999 = residents, 1000+ = staff.'] = '';
+l10n::$locales['Staff role:'] = '';
+l10n::$locales['staff role'] = '';
+l10n::$locales['Used only for staff member.'] = '';
+l10n::$locales['Dates of stay are not yet known.'] = '';
+l10n::$locales['Stay in the hostel from <strong>%s</strong> to <strong>%s</strong>'] = '';
+l10n::$locales['Welcome to the hostel %s!'] = '';
+l10n::$locales['You\'re staying in room number'] = '';
+l10n::$locales['You\'re staying in the <strong>staff</strong> room number'] = '';
+l10n::$locales['Your staff position is: <strong>%s</strong>'] = '';
+l10n::$locales['No room assigned yet.'] = '';
+l10n::$locales['Your dates of stay are not yet known.'] = '';
+l10n::$locales['You stay in the hostel from <strong>%s</strong> to <strong>%s</strong>'] = '';
+l10n::$locales['The email you use for this game is: <strong>%s</strong> (it will be known only to innkeepers).'] = '';
+l10n::$locales['<a href="%s">Contact the organizers of the game</a>'] = '';
+l10n::$locales['<a href="%s">Forum</a>'] = '';
+l10n::$locales['auberge'] = '';
+l10n::$locales['Staff'] = '';
+l10n::$locales['Staff member'] = '';
+l10n::$locales['Room %s'] = '';
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Room'] = 'Chambre';
+l10n::$locales['Check-in'] = 'Arrivée';
+l10n::$locales['Check-out'] = 'Départ';
+l10n::$locales['Staff role'] = 'Role';
+l10n::$locales['%Y-%m-%d'] = '%d-%m-%Y';
+l10n::$locales['Auberge'] = '';
+l10n::$locales['Room number:'] = 'Numéro de chambre :';
+l10n::$locales['0 = not set, 1 to 999 = residents, 1000+ = staff.'] = '0 = inconnu, 1 à 999 = résidents, 1000+ = membre du personnel';
+l10n::$locales['Staff role:'] = 'Rôle :';
+l10n::$locales['staff role'] = 'rôle';
+l10n::$locales['Used only for staff member.'] = 'utilisé uniquement pour les membres du personnel.';
+l10n::$locales['Dates of stay are not yet known.'] = 'Dates de séjour pas encore connues.';
+l10n::$locales['Stay in the hostel from <strong>%s</strong> to <strong>%s</strong>'] = 'Réside à l\'auberge du <strong>%s</strong> au <strong>%s</strong>';
+l10n::$locales['Welcome to the hostel %s!'] = 'Bienvenue à l\'auberge %s !';
+l10n::$locales['You\'re staying in room number'] = 'Vous logez dans la chambre numéro';
+l10n::$locales['You\'re staying in the <strong>staff</strong> room number'] = 'Vous logez dans la chambre <strong>du personnel</strong> numéro';
+l10n::$locales['Your staff position is: <strong>%s</strong>'] = 'Votre rôle est : <strong>%s</strong>';
+l10n::$locales['No room assigned yet.'] = 'Pas encore de chambre affectée.';
+l10n::$locales['Your dates of stay are not yet known.'] = 'Vos dates de séjour ne sont pas encore connues.';
+l10n::$locales['You stay in the hostel from <strong>%s</strong> to <strong>%s</strong>'] = 'Vous résidez à l\'auberge du <strong>%s</strong> au <strong>%s</strong>';
+l10n::$locales['The email you use for this game is: <strong>%s</strong> (it will be known only to innkeepers).'] = 'L\'email que vous utilisez pour le jeu est : <strong>%s</strong> (il ne sera connu que des aubergistes).';
+l10n::$locales['<a href="%s">Contact the organizers of the game</a>'] = '<a href="%s">Contacter les organisatrices du jeu</a>';
+l10n::$locales['<a href="%s">Forum</a>'] = '<a href="%s">Forum</a>';
+l10n::$locales['auberge'] = '';
+l10n::$locales['Staff'] = '';
+l10n::$locales['Staff member'] = 'Membre du personnel';
+l10n::$locales['Room %s'] = 'Chambre %s';
