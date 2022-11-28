309cee166686daa6f4c7a912179a9c85dba3b4cf
Franck <franck@open-time.net>
2022-11-28 07:24:20 +0100

    Cope with new install

--- _install.php
+++ _install.php
@@ -17,7 +17,7 @@ if (!defined('DC_CONTEXT_ADMIN')) {
 $new_version = dcCore::app()->plugins->moduleInfo('auberge', 'version');
 $old_version = dcCore::app()->getVersion('auberge');
 
-if (version_compare($old_version, $new_version, '>=')) {
+if (version_compare((string) $old_version, $new_version, '>=')) {
     return;
 }
 
